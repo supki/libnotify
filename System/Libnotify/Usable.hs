e5a9f19ba4956a7da0915bdc143e96771622c45b
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-01-26 22:55:57 +0400

    Made some corrections. Make pseudo-homogeneus hints lists possible.

--- System/Libnotify/Usable.hs
+++ System/Libnotify/Usable.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 module System.Libnotify.Usable
   ( withNotifications
-  , Hint, setHint
+  , setHint, generalize
   , oneShot) where
 
 import Data.Word (Word8)
@@ -29,20 +29,36 @@ oneShot t b i hs = withNotifications Nothing $ do n <- newNotify t b i
 
 type Key = String
 
+data GeneralHint = HintInt String Int | HintDouble String Double | HintString String String | HintByte String Word8 | HintArray String BS.ByteString
+
 class Hint a where
   setHint :: Notification -> a -> IO ()
+  generalize :: a -> GeneralHint
+
+instance Hint GeneralHint where
+  setHint n (HintInt k v) = setHint n (k,v)
+  setHint n (HintDouble k v) = setHint n (k,v)
+  setHint n (HintString k v) = setHint n (k,v)
+  setHint n (HintByte k v) = setHint n (k,v)
+  setHint n (HintArray k v) = setHint n (k,v)
+  generalize = id
 
 instance Hint (Key,Int) where
   setHint n (k,v) = setHintInt32 n k v
+  generalize (k, v) = HintInt k v
 
 instance Hint (Key,Double) where
   setHint n (k,v) = setHintDouble n k v
+  generalize (k, v) = HintDouble k v
 
 instance Hint (Key,String) where
   setHint n (k,v) = setHintString n k v
+  generalize (k, v) = HintString k v
 
 instance Hint (Key,Word8) where
   setHint n (k,v) = setHintByte n k v
+  generalize (k, v) = HintByte k v
 
 instance Hint (Key,BS.ByteString) where
   setHint n (k,v) = setHintByteArray n k v
+  generalize (k, v) = HintArray k v
